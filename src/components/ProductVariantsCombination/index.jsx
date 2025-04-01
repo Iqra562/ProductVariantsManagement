c2776571f0b48de802e476e3e9db8a825e7c490c
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-04-01 15:36:50 +0500

    add some validation

--- src/components/ProductVariantsCombination/index.jsx
+++ src/components/ProductVariantsCombination/index.jsx
@@ -1,23 +1,32 @@
 import React, { useContext, useEffect, useState } from 'react'
 import VariantsCombinationContext from '../../context/VariantsCombinationContextProvider';
 function ProductVariantsCombination(){
-    const {variantsCombination, setVariantsCombination} =useContext(VariantsCombinationContext); 
-    const getAllValues = variantsCombination.map(variant => variant.values);
-    const [variantCombination,setVariantCombination] = useState([])
+    const {variantsCombination } =useContext(VariantsCombinationContext); 
+    const getAllValues = variantsCombination.map(variant => variant.values).filter(values => values.length > 0); ;
+    // const [variantCombination,setVariantCombination] =  useState(() => {
+    //     const savedVariants = localStorage.getItem("combination");
+    //     return savedVariants ? JSON.parse(savedVariants) : [];
+    //   });
+    const [variantCombination,setVariantCombination] =  useState([]);
+    
     
     useEffect(()=>{
-        const combination =    getAllValues.reduce((a, b) => a.reduce((r, v) => r.concat(b.map(w => [].concat(v, w))), []));
+        const combination =  getAllValues.length >0 &&   getAllValues.reduce((a, b) =>  a.reduce((r, v) => r.concat(b.map(w => [].concat(v, w))), []))  ;
         setVariantCombination(combination)
         console.log(variantCombination)
+    // localStorage.setItem("combination", JSON.stringify(combination));
+        
+// console.log(getAllValues)
     },[variantsCombination])
     return(<>
     <div className='my-10 px-4'>
-{variantCombination.map((values)=>{
+ { variantCombination && ( variantCombination.map((values)=>{
+     const convertedValues = Array.isArray(values) ? values : [values];
     console.log(values,'v')
     return (
         <div className='border border-gray-300 p-5 rounded-md flex justify-between'>
         <div className='flex space-x-4'>
-          {values.map((value)=>(
+          {convertedValues.map((value)=>(
           <div className='bg-gray-300 px-2 py-1 text-gray-700 rounded capitalize text-sm'> {value}</div>
           ))}
             
@@ -26,7 +35,7 @@ function ProductVariantsCombination(){
       </div>
     )
  
-})}
+}))}
       
     </div>
     </>)
